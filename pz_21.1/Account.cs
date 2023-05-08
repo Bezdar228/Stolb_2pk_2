0d5231c528571b7b48c1e9b76196cb703bc5f0cb
Bezdar <not Founded>
2023-05-08 21:36:37 +0500

    -_-

---
+++ pz_21.1/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21._1
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+
+        public Account(int id, string username, string email, string password, DateTime registrationDate)
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            Password = password;
+            RegistrationDate = registrationDate;
+
+        }
+    }
+}
---
+++ pz_21.1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_21._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(
+                id: 228,
+                username: "Tomas Shelby",
+                email: "Bebra.1132.@mai.ru",
+                password: "154331trwqsaf12356236",
+                registrationDate: DateTime.Now);
+
+            Console.WriteLine($"Account Id: {account.Id}");
+            Console.WriteLine($"Username: {account.Username}");
+            Console.WriteLine($"Email: {account.Email}");
+            Console.WriteLine($"Password: {account.Password}");
+            Console.WriteLine($"Registration Date: {account.RegistrationDate}");
+
+        }
+    }
+}
