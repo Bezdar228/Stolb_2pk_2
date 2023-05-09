1aee9caae7e3f62d87ea499fe70522c631d8eee3
Bezdar <not Founded>
2023-05-09 13:14:08 +0500

    -_-

---
+++ pz_26.1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_26._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewFileMenuItemClick(object sender, RoutedEventArgs e)
+        {
+           
+           
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextField_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void TextField_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_Trash(object sender, RoutedEventArgs e)
+        {
+            mainRTB.Document.Blocks.Clear();
+        }
+
+        private void Button_Italic(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
