6bac7eb923397dc4b857cadd043690004c083cdc
Bezdar <not Founded>
2023-05-14 21:06:55 +0500

    -_-

---
+++ 28_1/Machine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_1
+{
+    internal class Machine
+    {
+
+        public event MyDelegate IncrSpeed;
+        public void Counter(int speed) => IncrSpeed?.Invoke(speed);
+    }
+}
---
+++ 28_1/Patrul.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_1
+{
+    internal class Patrul
+    {
+        public int chislo;
+        public Patrul(int speed)
+        {
+            chislo = speed;
+        }
+        public void show(int chislo)
+        {
+            while (chislo != 200)
+            {
+                chislo++;
+                Console.WriteLine($"Скорость равна {chislo}");
+                if (chislo > 80 & chislo < 120)
+                {
+                    Console.WriteLine("Снизьте немедленно скорость");
+                }
+                else if (chislo > 120)
+                {
+                    Console.WriteLine("Снизьте скорость или по вам будет открыт огонь");
+                }
+            }
+        }
+
+    }
+}
